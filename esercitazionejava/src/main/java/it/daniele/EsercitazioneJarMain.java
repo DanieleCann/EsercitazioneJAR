b2f6803826f09cbc1b1bfac8c78d471d52c09ef2
danielecannalire <bunker_king@hotmail.it>
2023-08-11 18:13:27 +0200

    feat: esempio log implementato

--- esercitazionejava/src/main/java/it/daniele/EsercitazioneJarMain.java
+++ esercitazionejava/src/main/java/it/daniele/EsercitazioneJarMain.java
@@ -1,7 +1,16 @@
 package it.daniele;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class EsercitazioneJarMain {
+    private static final Logger logger = LogManager.getLogger(EsercitazioneJarMain.class);
     public static void main(String[] args){
+        logger.info("applicazione si sta avviando correttamente");
+        logger.warn("le seguenti saranno una prova di log");
+        logger.debug("questo è un esempio di messaggio in debug");
+        logger.error("questo è un esempio di errore");
 
     }
 }
